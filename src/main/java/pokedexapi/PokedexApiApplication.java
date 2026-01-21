aeddf90c70623248866d964cb3264602b1241c9f
Michael Ball <aaronhunter@live.com>
2026-01-21 00:29:11 -0600

    server readiness wip4

--- src/main/java/pokedexapi/PokedexApiApplication.java
+++ src/main/java/pokedexapi/PokedexApiApplication.java
@@ -1,15 +1,25 @@
 package pokedexapi;
 
+import org.jspecify.annotations.NonNull;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-class PokedexApiApplication
+class PokedexApiApplication extends SpringBootServletInitializer
 {
     /* Main method */
-    public static void main(String[] args)
+    static void main(String[] args)
     {
         SpringApplication.run(PokedexApiApplication.class, args);
+    }
+
+    @NonNull
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder)
+    {
+        return builder.sources(PokedexApiApplication.class);
     }
 
 }
